69e8c900a4320db0501ac91602350e89d421de73
Kacp10 <145814779+kacp10@users.noreply.github.com>
2026-06-15 21:32:29 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import db_layer
 
 BASE = os.path.dirname(os.path.abspath(__file__))
 DB = os.path.join(BASE, 'lifeos.db')
-VERSION = 15  # debe coincidir con FRONT_V en static/app.js
+VERSION = 16  # debe coincidir con FRONT_V en static/app.js
 app = Flask(__name__)
 
 
